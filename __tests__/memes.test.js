f9e861762f48d25d6c30ffcb649f923190e79961
Jaime Sanders <jaimelyn517@gmail.com>
2020-06-30 17:57:33 -0700

    writes update a meme route and passes the test

--- __tests__/memes.test.js
+++ __tests__/memes.test.js
@@ -42,4 +42,25 @@ describe('meme routes', () => {
         expect(res.body).toEqual(meme);
       });
   });
+
+  it('updates a meme with PUT', async() => {
+    const meme = prepare(await Meme.findOne());
+
+    return await request(app)
+      .put(`/api/v1/memes/${meme._id}`)
+      .send({
+        top: 'shut up and',
+        image: 'https://i.kym-cdn.com/entries/icons/original/000/005/574/takemymoney.jpg',
+        bottom: 'take my money!',
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.anything(),
+          top: 'shut up and',
+          image: 'https://i.kym-cdn.com/entries/icons/original/000/005/574/takemymoney.jpg',
+          bottom: 'take my money!',
+          __v:0
+        });
+      });
+  });
 });
